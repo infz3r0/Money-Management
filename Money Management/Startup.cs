62dc6e639ea1389a34c87a6c9d426665f7e53df1
IFZR <infz3r0@gmail.com>
2018-08-09 21:22:38 +0700

    Add project files.

---
+++ Money Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Money_Management.Startup))]
+namespace Money_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
