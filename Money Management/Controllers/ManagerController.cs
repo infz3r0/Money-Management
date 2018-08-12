81536570a4467df147189e205fed0898b85c1d29
IFZR <infz3r0@gmail.com>
2018-08-12 15:42:41 +0700

    Register/Login

---
+++ Money Management/Controllers/ManagerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Money_Management.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            if (Session["Account"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+    }
+}
